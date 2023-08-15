78a43bbda43ff1c3fc9c4fe1adb924bc11f87684
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-08-15 16:03:04 +0200

    favbuttonworking

--- components/Spotlight/index.js
+++ components/Spotlight/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Image from "next/image";
 import FavoriteButton from "../FavoriteButton/FavoriteButton";
+import Link from "next/link";
 
-export default function Spotlight({ image, artist }) {
+export default function Spotlight({ image, artist, slug }) {
   return (
     <div className="spotlight">
       <Image src={image} alt={`${artist}`} width={300} height={300} />
