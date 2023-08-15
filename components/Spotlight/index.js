ae0ac2054bd4ff892868c2b5ecb8e4948978f7cf
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-08-15 09:45:15 +0200

    pushingminor

--- components/Spotlight/index.js
+++ components/Spotlight/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
-export function Spotlight({ image, artist }) {
+export default function Spotlight({ image, artist }) {
   return (
     <div className="spotlight">
       <Image src={image} alt={artist} width={300} height={300} />
