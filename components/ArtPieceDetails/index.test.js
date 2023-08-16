f36a58a9366c04c028ae504cfd7d12bc9605fb91
alihinnawe <hinnaweali@gmail.com>
2023-08-16 14:04:26 +0200

    repushing testing ArtPieceDetails

--- components/ArtPieceDetails/index.test.js
+++ components/ArtPieceDetails/index.test.js
@@ -5,26 +5,6 @@ import { useRouter } from "next/router";
 
 //render the ArtPieceDetails component with some test props and
 //then asserts that the expected elements are present in the document.
-describe("ArtPieceDetails", () => {
-  it("renders the art piece details correctly", () => {
-    const props = {
-      image: "/test-image.jpg",
-      title: "Test Title",
-      artist: "Test Artist",
-      year: "2023",
-      genre: "Test Genre",
-    };
-
-    render(<ArtPieceDetails {...props} />);
-
-    expect(screen.getByText("← Go Back to Gallery")).toBeInTheDocument();
-    expect(screen.getByAltText(props.title)).toBeInTheDocument();
-    expect(screen.getByText(props.artist)).toBeInTheDocument();
-    expect(screen.getByText(props.title)).toBeInTheDocument();
-    expect(screen.getByText(props.genre)).toBeInTheDocument();
-    expect(screen.getByText(props.year)).toBeInTheDocument();
-  });
-});
 
 ////////////////////////////////////////////////////////////////////
 //The test checks if the component renders the art piece details correctly,
@@ -32,15 +12,15 @@ describe("ArtPieceDetails", () => {
 //with the correct props, such as the source, height, and width.
 
 describe("ArtPieceDetails", () => {
-  it("renders the art piece details correctly", () => {
-    const props = {
-      image: "/test-image.jpg",
-      title: "Test Title",
-      artist: "Test Artist",
-      year: "2023",
-      genre: "Test Genre",
-    };
+  const props = {
+    image: "/test-image.jpg",
+    title: "Test Title",
+    artist: "Test Artist",
+    year: "2023",
+    genre: "Test Genre",
+  };
 
+  it("renders the art piece details correctly", () => {
     render(<ArtPieceDetails {...props} />);
 
     expect(screen.getByText("← Go Back to Gallery")).toBeInTheDocument();
@@ -52,14 +32,6 @@ describe("ArtPieceDetails", () => {
   });
 
   it("renders the image with the correct props", () => {
-    const props = {
-      image: "/test-image.jpg",
-      title: "Test Title",
-      artist: "Test Artist",
-      year: "2023",
-      genre: "Test Genre",
-    };
-
     render(<ArtPieceDetails {...props} />);
 
     const image = screen.getByAltText(props.title);
